c6e44b7fc9bf176c4e1c7676b1c86816436f9561
Christine Eriksen <christine.eriksen@gmail.com>
2022-12-07 15:05:03 -0500

    commit tree fa98bdef6a80b7f6ac9b071c0088ec58f9cf6054

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    employee0: {
+        name: "Peter",
+        streetAddress: "30 Dragon Ln",
+    employee1: {
+        name: "Jane",
+        streetAddress: "1 Shopshire St"
+    }
+    }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const keylessEmployee = {...employee};
+    delete keylessEmployee[key];
+    return keylessEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
